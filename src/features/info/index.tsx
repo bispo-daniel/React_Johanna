f59174145f31c8545eac46fa29a5aaddd535260d
Daniel Bispo <dbispo.work@gmail.com>
2024-05-25 22:36:15 -0300

    refactor: change the info page text

--- src/features/info/index.tsx
+++ src/features/info/index.tsx
@@ -4,40 +4,41 @@ function Info() {
   return (
     <main className="min-h-full flex flex-grow overflow-y-auto items-center justify-evenly gap-10 pt-4 md:mx-10 select-none flex-col md:flex-row">
       <div className="h-full flex flex-col items-start max-w-[500px] md:py-10">
-        <h1 className="text-2xl mb-4 font-semibold text-[#f5ac19]">
-          Sobre nós
-        </h1>
+        <h1 className="text-2xl mb-4 font-semibold text-[#f5ac19]">Sobre</h1>
         <p className="text-start font-light text-sm">
-          A Johanna foi criada como parte de um projeto universitário.{" "}
+          Johanna foi desenvolvida como parte de um projeto universitário para
+          auxiliar pessoas interessadas no curso de psicologia da UniDrummond e
+          que possuem dúvidas sobre ele.
         </p>
         <p className="text-start font-light text-sm pt-1">
-          A intenção é ajudar pessoas interessadas no curso de psicologia e que
-          têm dúvidas sobre ele. Nossa equipe trabalhou para fornecer dados
-          suficientes para que a Johanna tenha resposta para todas as suas
-          perguntas.
+          Nossa equipe trabalhou para fornecer dados suficientes para que
+          Johanna tenha respostas para todas as perguntas, inclusive aquelas
+          relacionadas à instituição de ensino.
         </p>
         <p className="text-start font-light text-sm pt-2">
-          Ela é uma aprendiz contínua. Portanto, sinta-se à vontade para
-          ensiná-la a qualquer momento...
+          Ela é uma aprendiz contínua, então sinta-se à vontade para ensiná-la a
+          qualquer momento.
         </p>
         <p className="text-start font-light text-sm pt-2">
-          A equipe é composta por 5 rapazes:
-        </p>
-        <p className="text-start font-light text-sm pt-1">
-          Rodrigo - Principalmente responsável pelo backend deste projeto.
-        </p>
-        <p className="text-start font-light text-sm pt-1">
-          Daniel - O cara que fez a interface que você está lendo.
-        </p>
-        <p className="text-start font-light text-sm pt-1">
-          Pedro, Julio e Gustavo - Responsáveis pelas outras partes do projeto.
+          Em relação ao website:
         </p>
         <p className="text-start font-light text-sm pt-2">
-          No frontend, React Vite em TypeScript foi a escolha.
+          No frontend, foi escolhido React Vite em TypeScript.
         </p>
         <p className="text-start font-light text-sm pt-1">
-          O backend foi feito em Java e integra-se com o Fine-Tuning da Open AI
-          e um banco de dados MySQL.
+          O backend foi dividido em duas partes:
+        </p>
+        <p className="text-start font-light text-sm pt-1">
+          Uma API REST em Java, responsável pela autenticação e manipulação dos
+          usuários.
+        </p>
+        <p className="text-start font-light text-sm pt-1">
+          Uma API WEB SOCKET em NodeJS, que integra a API da OpenAI e entrega
+          respostas instantâneas para o cliente.
+        </p>
+        <p className="text-start font-light text-sm pt-1">
+          Para armazenar todos os dados relevantes à aplicação, foi utilizado um
+          banco de dados MySQL.
         </p>
       </div>
 
